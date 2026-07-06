82f593194078713f2f4c61a3c918087dfe3ff4ab
lucaldev <lucaldev22@gmail.com>
2026-07-06 06:59:23 -0300

    Arrumando erro de whitespace indesejado.

--- app/[id]/historico/page.tsx
+++ app/[id]/historico/page.tsx
@@ -27,35 +27,37 @@ export default function Historico() {
       {error && (
         <div className="bg-red-500 w-fit text-white p-2 rounded">{error}</div>
       )}
-      <table className="w-full border-collapse">
-        <thead>
-          <tr className="border-b font-bold">
-            <th className="p-2 text-left">Rodada</th>
-            <th className="p-2 text-left">Jogador</th>
-            <th className="p-2 text-center">Pts Jogador</th>
-            <th className="p-2 text-left">Dealer</th>
-            <th className="p-2 text-center">Pts Dealer</th>
-            <th className="p-2 text-center">Resultado</th>
-            <th className="p-2 text-right">Apostado</th>
-          </tr>
-        </thead>
+      {historico.length > 0 ? (
+        <table className="w-full border-collapse">
+          <thead>
+            <tr className="border-b font-bold">
+              <th className="p-2 text-left">Rodada</th>
+              <th className="p-2 text-left">Jogador</th>
+              <th className="p-2 text-center">Pts Jogador</th>
+              <th className="p-2 text-left">Dealer</th>
+              <th className="p-2 text-center">Pts Dealer</th>
+              <th className="p-2 text-center">Resultado</th>
+              <th className="p-2 text-right">Apostado</th>
+            </tr>
+          </thead>
 
-        <tbody>
-          {historico.length > 0
-            ? historico.map((h) => (
-                <tr key={h.rodada} className="border-b">
-                  <td className="p-2">{h.rodada}</td>
-                  <td className="p-2">{h.jogador}</td>
-                  <td className="p-2 text-center">{h.pontos_jogador}</td>
-                  <td className="p-2">{h.dealer}</td>
-                  <td className="p-2 text-center">{h.pontos_dealer}</td>
-                  <td className="p-2 text-center">{h.resultado}</td>
-                  <td className="p-2 text-right">R$ {h.apostado}</td>
-                </tr>
-              ))
-            : ""}
-        </tbody>
-      </table>
+          <tbody>
+            {historico.map((h) => (
+              <tr key={h.rodada} className="border-b">
+                <td className="p-2">{h.rodada}</td>
+                <td className="p-2">{h.jogador}</td>
+                <td className="p-2 text-center">{h.pontos_jogador}</td>
+                <td className="p-2">{h.dealer}</td>
+                <td className="p-2 text-center">{h.pontos_dealer}</td>
+                <td className="p-2 text-center">{h.resultado}</td>
+                <td className="p-2 text-right">R$ {h.apostado}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
