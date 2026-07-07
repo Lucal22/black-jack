ae88ed952063f0d70d8d81ce90f5a9b531a65135
lucaldev <lucaldev22@gmail.com>
2026-07-07 15:14:55 -0300

    Simulação de uma partida pelo bd

--- docs/db/blackjack_comandos.sql
+++ docs/db/blackjack_comandos.sql
@@ -3,24 +3,33 @@ use blackjack;
 
 -- Rodada:
 
--- Cria rodada
-insert into rodada(ID_jogador,ID_dealer)
-values(1,10);
-select * from rodada;
-select * from jogador where id_jogador = 1;
-update carteira set saldo = 40 where ID_jogador = 101; 
-select * from carteira where ID_jogador = 101;
+-- Cria jogador
+insert into jogador(login, senha)
+values(Anna, 1234);
+select ID_jogador from jogador where login = Anna;
 
+-- Dados jogador
+select j.login, c.saldo from jogador as j
+join carteira as c 
+on j.ID_jogador = c.ID_jogador
+ where id_jogador = ?;
+
+-- Cria rodada
+call inicia_rodada(?);
+select * from rodada where resultado = "Andamento";
+
+-- Joga carta
 call joga_carta(2,'Jogador');
 call joga_carta(2,'Dealer');
 
-select * from carta_rodada where id_rodada = 4 order by data_insercao;
-select * from carteira_rodada;
-select * from carteira where ID_jogador = 1;
-
+-- Calcula total de pontos feitos
 select
     SUM(case when cr.dono = 'Jogador' then c.valor else 0 end) as Jogador,
     SUM(case when cr.dono = 'Dealer' then c.valor else 0 end) as Dealer
 from carta_rodada cr
 join carta c on c.ID_carta = cr.ID_carta
-where cr.ID_rodada = 4;
+where cr.ID_rodada = ?;
+
+-- Cartas inseridas
+select * from carta_rodada where id_rodada = ? order by data_insercao;
+
